8e4410f0d0bfd24c25ce052d1ff29a53750efa64
맹주영 <mjy9088@naver.com>
2022-05-25 02:21:24 +0900

    Initial commit

---
+++ src/ft_types.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_types.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 20:27:37 by jmaing            #+#    #+#             */
+/*   Updated: 2022/05/23 08:21:01 by Juyeong Maing    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TYPES_H
+# define FT_TYPES_H
+
+# include <stdbool.h>
+
+typedef bool			t_err;
+typedef unsigned char	t_byte;
+
+#endif
