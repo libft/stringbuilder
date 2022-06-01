bcdfc2732c68083014b0be1679945df1c8ef3990
맹주영 <mjy9088@naver.com>
2022-06-01 13:21:28 +0900

    silence test

--- test/case2/test.sh
+++ test/case2/test.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-find ../data/lines -type f -name "*.in.txt" | sed "s/\\.in\\.txt\$//" | while read file; do
-  echo "Processing $file.in.txt - 1024"
-  "./$1" 1024 "$file.in.txt"
-  echo "Processing $file.in.txt - 42"
-  "./$1" 42 "$file.in.txt"
-  echo "Processing $file.in.txt - 5"
-  "./$1" 5 "$file.in.txt"
+find ../data/lines -type f -name "*.in.txt" | while read file; do
+  "./$1" 1024 "$file"
+  "./$1" 42 "$file"
+  "./$1" 5 "$file"
+  "./$1" 1 "$file"
 done
